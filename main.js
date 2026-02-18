1453d1decb65466d24cbd5bf1ca76842da236c1e
ionut <rautuionut7@gmail.com>
2026-02-18 17:07:50 +0200

    made the getHumanChoice function
    this prompts the user to choos between three options

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ const getComputerChoice = () => {
   const options = ["rock", "paper", "scissors"]
   return options[Math.floor(Math.random() * 3)]
 }
+
+const getHumanChoice = () => prompt("Pick rock, paper or scissors")
