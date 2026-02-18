e3b8546911616c2ae4ddeef931a62cb2e659dbe9
ionut <rautuionut7@gmail.com>
2026-02-18 17:20:11 +0200

    defined the playRound function
    this has the logic for playing one round of the game

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ const getComputerChoice = () => {
 }
 
 const getHumanChoice = () => prompt("Pick rock, paper or scissors")
+
+humanScore = 0
+omputerScore = 0  
+
+const playRound = (humanChoice, computerChoice) => {
+  humanChoice = humanChoice.toLowerCase()
+
+  if (humanChoice === computerChoice) {
+    console.log("Draw!")
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("You win!")
+    humanScore++
+  } else {
+    console.log("You lose!")
+    computerChoice++
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice())
