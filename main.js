4e57054af000cc0833b573f0cba0b115214927b3
ionut <rautuionut7@gmail.com>
2026-02-20 00:23:38 +0200

    replaced all the console logs with a div displaying the outcome of the round

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 const getComputerChoice = () => {
-  const options = ["patra", "paper", "scissors"]
+  const options = ["rock", "paper", "scissors"]
   return options[Math.floor(Math.random() * 3)]
 }
 
-
-humanScore = 0
-computerScore = 0  
+let humanScore = 0
+let computerScore = 0  
 
 const rock = document.querySelector("#rock")
-const paper = ducunent.querySelector("#paper")
-const scissors = ducument.querySelector("scissors")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
 
-rock.addEventListener("click", (e) => playRound(getComputerChoice))
-paper.addEventListener("click", (e) => playRound(getComputerChoice))
-scissors.addEventListener("click", (e) => playRound(getComputerChoice))
+rock.addEventListener("click", () => playRound("rock", getComputerChoice()))
+paper.addEventListener("click", () => playRound("paper",getComputerChoice()))
+scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()))
 
-  const playRound = (computerChoice) => {
-  const humanChoice = e.target.textContent
+const result = document.querySelector(".result")
+
+const playRound = (humanChoice, computerChoice) => {
 
   if (humanChoice === computerChoice) {
-    console.log("Draw!")
+    result.textContent = "Draw!"
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
-    console.log("You win!")
+    result.textContent = "You win!"
     humanScore++
   } else {
-    console.log("You lose!")
+    result.textContent = "You lose!"
     computerScore++
   }
 }
